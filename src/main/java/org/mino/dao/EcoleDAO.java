86e42c6d555afdce40970997d71bb25b0fc78639
mino rafali <haingovalimbavaka@gmail.com>
2019-06-13 14:38:55 +0200

    Ajout d'une école "ecole_3" comme donnée de test

--- src/main/java/org/mino/dao/EcoleDAO.java
+++ src/main/java/org/mino/dao/EcoleDAO.java
@@ -13,9 +13,11 @@ public class EcoleDAO {
 		// initialisation des Ecoles
 		Ecole ecole1 = new Ecole(1, "Ecole_1");
 		Ecole ecole2 = new Ecole(2, "Ecole_2");
-		listEcole = new ArrayList<Ecole>();
+		Ecole ecole3 = new Ecole(3, "Ecole_32");
+		listEcole = new ArrayList<>();
 		listEcole.add(ecole1);
 		listEcole.add(ecole2);
+		listEcole.add(ecole3);
 	}	
 	
 	public List<Ecole> getListEcoles() {
